27fc67c083bb673f5e08aad2c9fd34a1bdebe2e8
Jay Ju <iam@juheejin.com>
2013-07-11 06:29:33 +0000

    Added fs & buf

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
+var str = fs.readFileSync('index.html');
+var buf = new Buffer(str, 'utf8');
 
 var app = express.createServer(express.logger());
-
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
