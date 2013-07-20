053e8eb52e61b3abcd68760030b3a9577c0a69d8
JayJu <iam@juheejin.com>
2013-07-20 07:28:21 +0000

    test

---
+++ test.js
@@ -0,0 +1,6 @@
+var rest = require('restler');
+var sys = require('util');
+//console.log(rest.get(String(1)).on('complete', function(data) {
+ // sys.puts(data[0].message); // auto convert to object
+//}));
+console.log(rest.get(String(1)));
